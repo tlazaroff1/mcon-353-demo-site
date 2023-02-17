9d0d16061749620dec50f720690c2232fe44e347
Tehila <tlazarof@student.touro.edu>
2023-02-16 20:14:16 -0500

    worked on todo

--- src/Components/todo/todo.jsx
+++ src/Components/todo/todo.jsx
@@ -3,11 +3,13 @@ import React, {useState} from 'react';
 
 export const Todo = () => {
     const[input, setInput] = useState("");
-    const [todos, setTodos]= useState(
-        [
-            
-        ]
-    );
+    const [todos, setTodos]= useState([{
+        title: 'first',
+        isComplete: false,
+    }, {
+        title: 'Second',
+        isComplete: true,
+    }]);
 
     const onInput = (event) =>{
         console.log(event.target.value);
@@ -16,20 +18,30 @@ export const Todo = () => {
 
     const addTodo = () => {
         //todos.push(input)
-        setTodos([...todos, input]);
-        console.log(todos);
+        setTodos([...todos, {title: input, isComplete: false}]);
+        setInput(" "); 
     };
 
+    const toggleChecked = (todo) => {
+       // todo.isComplete = !todo.isComplete;
+        const newTodos = [...todos];
+        const updatedTodo = newTodos.find((x) => x.title === todo.title);
+        updatedTodo.isComplete = !todo.isComplete;
+        setTodos(newTodos);
+
+    }
     return (
         <> 
             <h1>Todos</h1>
-            <input onInput={onInput}/>
+            <input onInput={onInput} value={input}/>
             <button onClick={addTodo}>Add</button>
             {
-                todos.map(todo => (
-                <p>
-                    <input type="checkbox"/>
-                    {todo}
+                todos.map((todo, index) => (
+                <p key={index}>
+                    <input type="checkbox" 
+                        checked={todo.isComplete} 
+                        onInput={() => toggleChecked(todo)}/>
+                    {todo.title}
                 </p>))
             }
             
